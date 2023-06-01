87f582c229ecf66c496e57c8e92d917f3b702b48
leeellen <lee.ellen0814@gmail.com>
2023-06-01 18:11:37 +0900

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: 'http://localhost:3000',
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('connection');
+});
+
+httpServer.listen(4000);
