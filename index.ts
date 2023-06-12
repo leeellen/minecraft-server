5c50312e611beea67c80b464318d028652efec19
leeellen <lee.ellen0814@gmail.com>
2023-06-12 19:10:45 +0900

    fix : socket setting

--- index.ts
+++ index.ts
@@ -2,25 +2,42 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 
 const httpServer = createServer();
-const io = new Server(httpServer, {
+const ioServer = new Server(httpServer, {
     cors: {
         origin: '*',
     },
 });
 
-let clients = 0;
+let clients = {};
 
 //Whenever someone connects this gets executed
-io.on('connection', function (socket) {
-    clients++;
-    console.log('A user connected');
+ioServer.on('connection', function (client) {
+    console.log(`User ${client.id} connected, there are currently ${ioServer.engine.clientsCount} users connected`);
 
-    io.sockets.emit('broadcast', clients);
+    //Add a new client indexed by his id
+    clients[client.id] = {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0],
+    };
 
-    socket.on('disconnect', function () {
-        clients--;
-        console.log('A user disconnected');
-        io.sockets.emit('broadcast', clients);
+    ioServer.sockets.emit('move', clients);
+
+    client.on('move', ({ id, rotation, position }) => {
+        clients[id].position = position;
+        clients[id].rotation = rotation;
+
+        ioServer.sockets.emit('move', clients);
+    });
+
+    client.on('disconnect', () => {
+        console.log(
+            `User ${client.id} disconnected, there are currently ${ioServer.engine.clientsCount} users connected`,
+        );
+
+        //Delete this client from the object
+        delete clients[client.id];
+
+        ioServer.sockets.emit('move', clients);
     });
 });
 
